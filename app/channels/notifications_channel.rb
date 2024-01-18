98dabcc8a7c13b22108f1ca72e29a2ec5d7b57aa
Thomas Escolastico Bueno <thomas.bueno@umanni.com.br>
2024-01-18 00:49:26 -0300

    creating notification channel

---
+++ app/channels/notifications_channel.rb
@@ -0,0 +1,10 @@
+class NotificationsChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "notifications_#{params[:user_id]}"
+  end
+ 
+ 
+  def unsubscribed
+    stop_all_streams
+  end
+ end
