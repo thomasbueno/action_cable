f10b7a6c8f1c6d55991230a802435e05530ba1ea
Thomas Escolastico Bueno <thomas.bueno@umanni.com.br>
2024-01-18 01:52:45 -0300

    add config to action cable url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,11 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.action_cable.url = "ws://localhost:3000/cable"
+
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, 
+  /https:\/\/*/]
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
